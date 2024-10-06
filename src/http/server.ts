38e42a2d540c76f2197037b54f84fbb14af7a522
skrodrigo <rodrigoa0987@gmail.com>
2024-10-05 22:56:05 -0300

    feat: add port of render

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,19 @@ import fastify from 'fastify'
 import { newsRoutes } from '../routes/news-routes'
 
 const app = fastify()
+
+// Registrar as rotas.
 app.register(newsRoutes)
 
-app.listen({ port: 3333 }).then(() => console.log('server up!'))
+// Definir a porta e o host a partir das variáveis de ambiente.
+const PORT = process.env.PORT ? Number.parseInt(process.env.PORT) : 3333
+const HOST = '0.0.0.0'
+
+// Iniciar o servidor no host e porta corretos.
+app
+  .listen({ port: PORT, host: HOST })
+  .then(() => console.log(`Server up and running on http://${HOST}:${PORT}`))
+  .catch(err => {
+    console.error('Error starting server:', err)
+    process.exit(1)
+  })
